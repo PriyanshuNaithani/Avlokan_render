25aed0d08793dd64feac47349b2e9b3085db1781
priyanshu naithani <naithanip000@gmail.com>
2023-07-27 17:14:19 +0530

    add required field msg in postBlog api

--- controllers/blog.js
+++ controllers/blog.js
@@ -2,6 +2,9 @@ const Blog = require("../models/blog");
 const postBlog = async (req, res) => {
     try {
         const newPost = new Blog(req.body);
+        if(!req.body.title || !req.body.text){
+          return res.status(200).send({success: false, msg: "kindly fill all the entries."})
+        }
         await newPost.save();
         res.status(200).send({success: true, msg:"Blog saved successfully."});
       } catch (error) {
